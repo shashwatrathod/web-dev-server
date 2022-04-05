07e32c2497dabbaa7a396b59386c5b5a4ffaf4ae
Shashwat <shashwatrathod22@gmail.com>
2022-04-05 09:02:55 -0400

    DELETE user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,6 +4,7 @@ const userController = (app) => {
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:uid", findUsersById);
   app.post("/api/users", createUser);
+  app.delete("/api/users/:uid", deleteUser);
 };
 
 const findAllUsers = (req, res) => {
@@ -31,6 +32,12 @@ const createUser = (req, res) => {
   newUser._id = new Date().getTime() + "";
   users.push(newUser);
   res.json(newUser);
+};
+
+const deleteUser = (req, res) => {
+  const userId = req.params["uid"];
+  users = users.filter((usr) => usr._id !== userId);
+  res.sendStatus(200);
 };
 
 export default userController;
